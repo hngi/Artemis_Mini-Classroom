c766c9fac13db3ab25826c67527c534491ee6bb5
charlene04 <charlesugbana04@gmail.com>
2019-09-26 15:53:41 +0100

    updated scripts

--- js/classList.js
+++ js/classList.js
@@ -4,20 +4,13 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('#dropbutton')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
+function myFunction_2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
   }
-
+function myFunction_3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+  }
+  // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -30,3 +23,6 @@ function myFunction() {
       }
     }
   }
+
+  
+  
