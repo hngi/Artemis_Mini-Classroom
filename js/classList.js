8a22bfdbed007afb60320838062a410c1dca61ec
oluwamayowaf <e_fadairo@yahoo.com>
2019-09-26 16:50:01 +0100

    Create class and class list modification files added with this commit

--- js/classList.js
+++ js/classList.js
@@ -4,13 +4,20 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-function myFunction_2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-  }
-function myFunction_3() {
-    document.getElementById("myDropdown3").classList.toggle("show");
-  }
   // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('#dropbutton')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -23,6 +30,3 @@ function myFunction_3() {
       }
     }
   }
-
-  
-  
