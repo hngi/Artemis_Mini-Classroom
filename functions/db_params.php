ce3ec12a9e75bf23c37b79b377ad8f1ba867862f
oluwamayowaf <e_fadairo@yahoo.com>
2019-09-29 00:23:36 +0100

    updated file

--- functions/db_params.php
+++ functions/db_params.php
@@ -3,6 +3,6 @@
     //Database Constants
     define("DB_SERVER", "localhost");
     define("DB_USER", "root");
-    define("DB_PASS", "");
+    define("DB_PASS", "miniclassroom");
     define("DB_NAME", "id11003649_miniclassroom");
 ?>
