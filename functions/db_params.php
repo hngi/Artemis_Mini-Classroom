5e1f0a50e31d318d2ff34db2ef17f6d7dfee57b0
Eferha <eferhamamus@gmail.com>
2019-10-04 13:49:08 +0100

    contactuspage

--- functions/db_params.php
+++ functions/db_params.php
@@ -1,8 +1,13 @@
 <?php
     error_reporting(1);
     //Database Constants
+    // define("DB_SERVER", "localhost");
+    // define("DB_USER", "id11003649_miniclassroom");
+    // define("DB_PASS", "miniclassroom");
+    // define("DB_NAME", "id11003649_miniclassroom");
+
     define("DB_SERVER", "localhost");
-    define("DB_USER", "id11003649_miniclassroom");
-    define("DB_PASS", "miniclassroom");
-    define("DB_NAME", "id11003649_miniclassroom");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("DB_NAME", "miniclassroom");
 ?>
