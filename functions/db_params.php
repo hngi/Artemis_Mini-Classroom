14ad4b40ce1acbfa18236627c74fd29b12aec30d
Oluwamayowa Fadairo <54279480+OluwmayowaF@users.noreply.github.com>
2019-10-04 17:06:47 +0100

    Revert "contactuspage"

---
+++ functions/db_params.php
@@ -0,0 +1,8 @@
+<?php
+    error_reporting(1);
+    //Database Constants
+    define("DB_SERVER", "localhost");
+    define("DB_USER", "id11003649_miniclassroom");
+    define("DB_PASS", "miniclassroom");
+    define("DB_NAME", "id11003649_miniclassroom");
+?>
